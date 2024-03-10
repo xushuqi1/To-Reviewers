6a22586d56dcf11eb149c170d8c3d923ab944297
xushuqi1 <154055536+xushuqi1@users.noreply.github.com>
2024-03-10 17:09:11 +0100

    Add files via upload

---
+++ Code/input_orientedmodel.m
@@ -0,0 +1,27 @@
+function [output] = input_orientedmodel(P, xx, yy)
+    X = P(:, 1:xx);
+    Y = P(:, xx+1:xx+yy);
+    n = size(X, 1);
+
+    % 初始化存储目标函数最优值的向量
+    optimalValues = zeros(1, n);
+
+    for k = 1:n
+        c = [zeros(n, 1); 1];
+        A = [X', -X(k, :)'; -Y', zeros(yy, 1)];
+        b = [zeros(xx, 1); -Y(k, :)'];
+        Aeq = [ones(1, n), 0];
+        beq = 1;
+        lb = zeros(n+1, 1);
+        ub = [];
+        op = optimoptions('linprog', 'display', 'none');
+        
+        % 解决线性规划问题
+        [sol, fval] = linprog(c, A, b, Aeq, beq, lb, ub, op);
+
+        % 收集目标函数的最优值
+        optimalValues(k) = fval;
+    end
+
+    output = optimalValues;
+end
