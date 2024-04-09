9565de30ff7d52a1185bbb6b6f77fe0701b84b2c
xushuqi1 <154055536+xushuqi1@users.noreply.github.com>
2024-04-09 20:09:22 +0200

    Update First_phase_enhancedEHD_Parallel.m

--- Code/First_phase_enhancedEHD_Parallel.m
+++ Code/First_phase_enhancedEHD_Parallel.m
@@ -1,4 +1,4 @@
-function [output] = First_phase_enhancedEHD_Parallel(P, xx, yy, a)
+function [output] = First_phase_enhancedEHD_Parallel(P, xx, yy, a)              % a=int(n^0.5) 
 
     E_EHD = Function_initial_step_enhanced_EHD (P, xx, yy, a);            
     score_E_EHD = input_orientedmodel(E_EHD, xx, yy);
