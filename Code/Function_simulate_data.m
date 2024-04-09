2fcd1980b90966cc985106393c4acab8204389d1
xushuqi1 <154055536+xushuqi1@users.noreply.github.com>
2024-04-09 20:00:04 +0200

    Update Function_simulate_data.m

--- Code/Function_simulate_data.m
+++ Code/Function_simulate_data.m
@@ -1,4 +1,4 @@
-% This is the function code for generating data
+% This is the function code for generating data with controlled environment
 function [output]=Function_simulate_data(n,xx,yy,A)       %  n refers to the number of DMU
 % xx refers to the number of inputs, yy refers to the number of outputs
 % A refers to the density
