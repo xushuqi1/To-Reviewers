b68424e1014572e4918e2e3bfea6ad2bda5118dd
xushuqi1 <154055536+xushuqi1@users.noreply.github.com>
2024-04-09 20:08:39 +0200

    Update Function_initial_step_enhanced_EHD.m

--- Code/Function_initial_step_enhanced_EHD.m
+++ Code/Function_initial_step_enhanced_EHD.m
@@ -1,6 +1,6 @@
 function y=Function_initial_step_enhanced_EHD(P,xx,yy,a)  
 y_initial=twoD(P,xx,yy);
-y_second=EHD2019(P,xx,yy,a);
+y_second=Function_EHD2019_initial_step(P,xx,yy,a);
 y=[y_initial;y_second]
 y=unique(y,'rows'); 
 end
