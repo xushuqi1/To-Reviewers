62e00a2af9ca4d33775c6171f45f31d524af7799
xushuqi1 <154055536+xushuqi1@users.noreply.github.com>
2026-03-30 11:44:10 +0800

    Delete Code directory

--- Code/Function_initial_step_enhanced_EHD.m
+++
@@ -1,7 +0,0 @@
-function y=Function_initial_step_enhanced_EHD(P,xx,yy,a)    %   a=int(n^0.5) 
-y_initial=twoD(P,xx,yy);
-y_second=Function_EHD2019_initial_step(P,xx,yy,a);
-y=[y_initial;y_second]
-y=unique(y,'rows'); 
-end
-
